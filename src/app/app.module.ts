e98917672d31d36eb6bc7a3b61397dee3c081e2d
Youssef MAHTAT <Youssef.MAHTAT@gmail.com>
2019-04-28 18:54:26 +0100

    fixe layout-module with nav-top

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {LayoutModule} from './layout/layout.module';
+import {RouterModule, Routes} from '@angular/router';
+
+const appRoutes: Routes = [];
 
 @NgModule({
   declarations: [
@@ -10,7 +13,11 @@ import {LayoutModule} from './layout/layout.module';
   ],
   imports: [
     LayoutModule,
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    ),
   ],
   providers: [],
   bootstrap: [AppComponent]
